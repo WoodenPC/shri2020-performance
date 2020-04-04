0349f4156f33a7852a7e9540129a717aac5089e5
Arslanov Rasul <stiflerovich@mail.ru>
2020-04-04 18:30:09 +0500

    отмена изменений в index.html

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const purgecss = require('gulp-purgecss');
+const cleanCss = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const imgCompress  = require('imagemin-jpeg-recompress');
+
+gulp.task('purgecss', () => {
+  return gulp.src('css/**/*.css')
+    .pipe(purgecss({
+        content: ['index.html']
+    }))
+    .pipe(cleanCss())
+    .pipe(gulp.dest('build/css'))
+});
+
+gulp.task('compressImages', () => {
+  return gulp.src('img/**/*')
+    .pipe(imagemin([
+      imgCompress({
+        loops: 4,
+        min: 70,
+        max: 80,
+        quality: 'high'
+      }),
+      imagemin.gifsicle(),
+      imagemin.optipng({
+        optimizationLevel: 5,
+      }),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest('compressedImages'));
+})
