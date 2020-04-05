3f2f57cac93c1b27ac894fec896b4f20b62c7603
Arslanov Rasul <stiflerovich@mail.ru>
2020-04-05 15:44:12 +0500

    расскоментил код

--- js/index.js
+++ js/index.js
@@ -3,6 +3,6 @@ window.addEventListener('DOMContentLoaded', () => {
     multimediaScript.setAttribute('src','./js/multimedia.js');
     const buttonsContainer = document.querySelector('.buttons-wrap');
 
-    // buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
-    //     '<button class="button">Нет</button>';
+    buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
+        '<button class="button">Нет</button>';
 });
